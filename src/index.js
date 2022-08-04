bff99e22f9ce0e8264694a3f6da76959e76a616f
Judith van Leersum <judithvleersum@gmail.com>
2022-08-04 13:13:38 +0200

    Set up context provider for productsList

--- src/index.js
+++ src/index.js
@@ -1,22 +1,16 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import { Provider } from 'react-redux';
-import { combineReducers, createStore } from 'redux';
-import { BrowserRouter } from 'react-router-dom';
+import React from "react";
+import ReactDOM from "react-dom";
+import { BrowserRouter } from "react-router-dom";
 
-import './index.css';
-import App from './App';
-import productReducer from './store/reducers/products';
+import "./index.css";
+import App from "./App";
+import ProductsProvider from "./context/products-context";
 
-const rootReducer = combineReducers({
-  shop: productReducer
-});
-
-const store = createStore(rootReducer);
-
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(<Provider store={store}>
-  <BrowserRouter>
-    <App />
-  </BrowserRouter>
-</Provider>);
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(
+  <ProductsProvider>
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
+  </ProductsProvider>
+);
